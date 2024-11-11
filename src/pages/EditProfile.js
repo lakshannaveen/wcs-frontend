298e16003c55af3cc1d31568f7f3a33d3a58c6fe
Dulina033 <dulinaranasinghe03@gmail.com>
2024-11-11 08:47:16 +0530

    Edite Profile creater

---
+++ src/pages/EditProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from "../components/Profile";
+
+function EditProfile() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
+
+export default EditProfile;
