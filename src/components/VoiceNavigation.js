1037ac89a4e9e7fe3da51c6e83d8dd959f7aaa43
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2024-11-15 20:46:04 +0530

    updated social media pages

--- src/components/VoiceNavigation.js
+++ src/components/VoiceNavigation.js
@@ -22,7 +22,7 @@ const VoiceNavigation = () => {
       const transcript = event.results[event.results.length - 1][0].transcript.trim().toLowerCase();
       console.log("Heard:", transcript);
 
-      // navigation part
+      // Navigation within your app
       if (transcript.includes("go to home")) {
         navigate('/');
       } else if (transcript.includes("go to about")) {
@@ -35,8 +35,19 @@ const VoiceNavigation = () => {
         navigate('/teamsandconditions');
       } else if (transcript.includes("go to privacy")) {
         navigate('/privacy');
-      } else if (transcript.includes("go to profile")) {  // Changed to "go to profile"
+      } else if (transcript.includes("go to profile")) { 
         navigate('/customprofile');
+      }
+
+      // navigate to social media pages
+      else if (transcript.includes("go to facebook")) {
+        window.open("https://www.facebook.com/profile.php?id=61567165493241&mibextid=ZbWKwL");
+      } else if (transcript.includes("go to youtube")) {
+        window.open("https://youtube.com/@wcs-x3?si=lRB3Qz9z4pqeObtO");
+      } else if (transcript.includes("go to instagram")) {
+        window.open("https://www.instagram.com/wcs_08/profilecard/?igsh=MWpmbHh1NTcxb3ZqOA==");
+      } else if (transcript.includes("go to tiktok")) {
+        window.open("https://tiktok.com/@WCS_08");
       }
     };
 
