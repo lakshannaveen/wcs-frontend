59d021beca84554372e3c408d0ac3e6732bf6f17
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2024-11-17 21:02:54 +0530

    card reponsive issue solve

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,51 @@
-import React from 'react'//carosule import
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import carosle1 from '../images/carosle1.jpg';
 import carosle2 from '../images/carosle2.jpg';
 import carosle3 from '../images/carosle3.jpg';
 
 import './Home.css';
-//card import
+// card import
 import Card from 'react-bootstrap/Card';
-import {  Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import card1 from '../images/card1.jpeg';
 import card2 from '../images/card2.jpeg';
 import card3 from '../images/card3.jpeg';
 import Search from '../components/Search';
 import { Link } from 'react-router-dom';
-
-
-
-
-
+import { Link as ScrollLink } from 'react-scroll';
 function CustomHome() {
   return (
     <div>
-      {/* carousel section*/}
+      {/* carousel section */}
       <Carousel>
-      <Carousel.Item>
-        <img 
-        className='d-block w-100' src={carosle1} alt='First slide'>
-        </img>
-       </Carousel.Item>
-      <Carousel.Item>
-      <img className='d-block w-100' src={carosle2} alt='Second slide'>
-      </img>
-       </Carousel.Item>
-      <Carousel.Item>
-      <img className='d-block w-100' src={carosle3} alt='Third slide'>
-      </img>
+        <Carousel.Item>
+          <img className='d-block w-100' src={carosle1} alt='First slide' />
         </Carousel.Item>
-    </Carousel>
-   
-<Search /> {/* search.js page  */}
+        <Carousel.Item>
+          <img className='d-block w-100' src={carosle2} alt='Second slide' />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src={carosle3} alt='Third slide' />
+        </Carousel.Item>
+      </Carousel>
 
-      {/* card section*/}
+    
+      <div id="search-section">
+        <Search /> {/* Search.js page */}
+      </div>
+
+      {/* card section */}
       <Row className='custom-card-group'>
-        <Col md= {3} className="d-flex justify-content-center">
-        <Link to='/subscriptionplans' className='card-link'>
+        <Col md={3} className="d-flex justify-content-center">
+          <Link to='/subscriptionplans' className='card-link'>
             <Card className='custom-card'>
               <Card.Img variant="top" src={card1} alt='subscription' />
             </Card>
           </Link>
         </Col>
-        <Col md= {3} className="d-flex justify-content-center">
-        <Link to='/specialoffers' className='card-link'>
+        <Col md={3} className="d-flex justify-content-center">
+          <Link to='/specialoffers' className='card-link'>
             <Card className='custom-card'>
               <Card.Img variant="top" src={card2} alt='offers' />
             </Card>
@@ -64,19 +59,26 @@ function CustomHome() {
           </Link>
         </Col>
       </Row>
-      {/*commitement section*/}
+
+      {/* commitment section */}
       <section className='commitment-section'>
         <div className='container'>
           <h2>
-          Our Waste Collection Services are 100% Committed to Your Community!</h2>
+            Our Waste Collection Services are 100% Committed to Your Community!
+          </h2>
           <p>Eco-Friendly, Reliable, and Tailored Just for You!</p>
           <p className='highlight'>Together, We're Building a Cleaner Tomorrow!</p>
-          <button className='order-now-btn'>Order Now</button> 
+          <ScrollLink
+            to="search-section" 
+            smooth={true} 
+            duration={100}//we can increase the smooth
+          >
+            <button className='order-now-btn'>Order Now</button>
+          </ScrollLink>
         </div>
       </section>
-     
     </div>
-  )
+  );
 }
 
-export default CustomHome
+export default CustomHome;
