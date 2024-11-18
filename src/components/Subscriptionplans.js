125b16c638c9daf04cf1042b31d53361f71c781b
Dunira01 <duniranimthaka@gmail.com>
2024-11-18 11:11:11 +0530

    updated again

--- src/components/Subscriptionplans.js
+++ src/components/Subscriptionplans.js
@@ -5,11 +5,12 @@ function CustomSubscription() {
   return (
     <body>
       <div className="subscription-container">
-        <h1 className="subscription-title">SUBSCRIPTION PLAN</h1>
+        <h1 className="subscription-title">SUBSCRIPTION PLANS</h1><br></br><br></br>
+
         <div className="plans-wrapper">
           
           <div className="plan daily">
-            <h2><strong>Daily Collection</strong></h2>
+            <h2><strong>Daily Collection Plan</strong></h2>
             <ul>
               <li>• Daily waste collection.</li>
               <li>• On-demand pickups within service hours.</li>
@@ -20,7 +21,7 @@ function CustomSubscription() {
 
          
           <div className="plan weekly">
-            <h2><strong>Weekly Collection</strong></h2>
+            <h2><strong>Weekly Collection Plan</strong></h2>
             <ul>
               <li>• Scheduled weekly waste collection.</li>
               <li>• Priority scheduling for additional pickups.</li>
@@ -30,7 +31,7 @@ function CustomSubscription() {
 
           
           <div className="plan monthly">
-            <h2><strong>Monthly Collection</strong></h2>
+            <h2><strong>Monthly Collection Plan</strong></h2>
             <ul>
               <li>• Multiple collections per month.</li>
               <li>• Priority customer support.</li>
