0a027103127363507c9f52a51281e98f2ac2f05c
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2024-12-19 20:28:41 +0530

    login page created

---
+++ src/pages/Log_in.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomLogin from '../components/Login'
+
+function Log_in() {
+  return (
+    <div>
+      <CustomLogin />
+    </div>
+  )
+}
+
+export default Log_in
