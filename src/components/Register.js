7c40e8af588bf7a03ae93199d920a9880f923705
Yesandu Sithum <yesandusithumsamaka@gmail.com>
2025-01-22 14:52:59 +0530

    Register Form
    Without Validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,61 @@
-import React from 'react'
+// Register.js
+import React from 'react';
 import './Register.css';
+import { FaUser, FaLock, FaEnvelope  } from "react-icons/fa";
+
 
 function CustomRegister() {
   return (
-    <div>
-      welcome to register form
+    <div className='regform'>
+      <form className='registerForm'>
+        <h1>Register</h1>
+        <div className='formBody'>
+          <div>
+            <div className='Box'>
+              <select name='title' className='dropdown' required>
+                <option value='' disabled selected>Title</option>
+                <option value='Mrs'>Mrs</option>
+                <option value='Ms'>Ms</option>
+              </select>
+            </div>
+
+            <div className='Box'>
+              <input type="text" name='firstname' placeholder='First Name' required />
+              <FaUser className='icon' />
+            </div>
+
+            <div className='Box'>
+              <input type="text" name='lastname' placeholder='Last Name' required />
+              <FaUser className='icon' />
+            </div>
+
+            <div className='Box'>
+              <input type="text" name='username' placeholder='Username' required />
+              <FaUser className='icon' />
+            </div>
+
+            <div className='Box'>
+              <input type="email" name='email' placeholder='Email' required />
+              <FaEnvelope className='icon' />
+            </div>
+
+            <div className='Box'>
+              <input type='password' name='createpassword' placeholder='Create Password' required />
+              <FaLock className='icon' />
+            </div>
+
+            <div className='Box'>
+              <input type='password' name='confirmpassword' placeholder='Confirm Password' required />
+              <FaLock className='icon' />
+            </div>
+
+            <button className="button" type="submit"> Submit</button>
+            
+          </div>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default CustomRegister
+export default CustomRegister;
