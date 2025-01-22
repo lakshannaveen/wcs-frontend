0dfbd5f95335a03b7ec1041193105ab4d2c9e9e8
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-01-22 14:21:37 +0530

    login validated and updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Login.css';
-import { FaUser, FaLock, FaFacebook } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-
-
 
 function CustomLogin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState({ username: '', password: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate username and password
+    let errors = {};
+    if (username.trim() === '') {
+      errors.username = 'Username is required';
+    }
+    if (password.length < 8) {
+      errors.password = 'Password must be at least 8 characters long';
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setErrorMessage(errors);
+      return;
+    }
+
+    // If validation passes, clear the error messages
+    setErrorMessage({});
+    alert('Login successful!');
+    // Perform login logic (e.g., API call)
+  };
+
   return (
-    <div className='logform'>
-    <form className='loginForm'> 
-      <h1>Login</h1>
-      <div className='formBody'>
-        <div>
-          <div className='Box'>
-            <input type="text" name='username' placeholder='Username' required /> 
-            <FaUser className='icon' />
+    <div className="logform">
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className="formBody">
+          <div className="Box">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+              style={{
+                borderColor: errorMessage.username ? 'red' : '#ccc',
+              }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            {errorMessage.username && (
+              <span className="error">{errorMessage.username}</span>
+            )}
           </div>
-          <div className='Box'>
-            <input type='password' name='password' placeholder='Password' required />
-            <FaLock className='icon'/>
+          <div className="Box">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              style={{
+                borderColor: errorMessage.password ? 'red' : '#ccc',
+              }}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            {errorMessage.password && (
+              <span className="error">{errorMessage.password}</span>
+            )}
           </div>
-        
-          <div className='rem'>
-            <label><input type='checkbox' name='Remember' /> Remember Me </label>
-            <a href='#'>Forgot Password ?</a>
-          </div>
-
-          <button className="button" type="submit"> Login</button>
-
+          <button className="button" type="submit">Login</button>
           <div className="register">
-            <label> Don't have an account? </label>
-            <a href='#'>Register</a> <br /><br /><br />
-          </div>
-
-          <div> 
-            <button className="loginbutton" type="submit"> 
-              <FcGoogle className="gicon" />Login with Google
-            </button>
-          </div>
-          <br />
-
-          <div>
-            <button className='fbbutton' type='submit' name='fbbutton'> 
-              <FaFacebook /> Login with Facebook
-            </button>
+            <label>Don't have an account? </label>
+            <a href="register">Register</a>
           </div>
         </div>
-      </div>
-    </form>
-  </div>
-  )
+      </form>
+    </div>
+  );
 }
 
-export default CustomLogin
+export default CustomLogin;
