8eee4680bf489e2154891be82ce761a045dcfd55
lakshannaveen <139128804+lakshannaveen@users.noreply.github.com>
2025-01-28 18:08:50 +0530

    updated style of contact us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,11 +18,11 @@ const CustomContact = () => {
 
   const validate = () => {
     let tempErrors = {};
-    if (!formData.firstName) tempErrors.firstName = 'First Name is required';
-    if (!formData.lastName) tempErrors.lastName = 'Last Name is required';
-    if (!formData.email || !/\S+@\S+\.\S+/.test(formData.email)) tempErrors.email = 'Valid Email is required';
-    if (!formData.phone || formData.phone.length !== 10) tempErrors.phone = 'Valid Phone Number is required (10 digits)';
-    if (!formData.message) tempErrors.message = 'Message cannot be empty';
+    if (!formData.firstName) tempErrors.firstName = "First Name is required";
+    if (!formData.lastName) tempErrors.lastName = "Last Name is required";
+    if (!formData.email || !/\S+@\S+\.\S+/.test(formData.email)) tempErrors.email = "Valid Email is required";
+    if (!formData.phone || formData.phone.length !== 10) tempErrors.phone = "Valid Phone Number is required (10 digits)";
+    if (!formData.message) tempErrors.message = "Message cannot be empty";
     setErrors(tempErrors);
     return Object.keys(tempErrors).length === 0;
   };
@@ -68,13 +68,14 @@ const CustomContact = () => {
         console.error('Error submitting form:', error);
         setErrorMessage('An error occurred while submitting the form. Please try again later.');
       }
+      setValidated(true); 
     }
     setValidated(true); 
   };
 
   return (
-    <div className="contactus">
-      <div className="left-section">
+    <div className='contactus'>
+      <div className='left-section'>
         <h2>Contact Information</h2>
         <p>Feel Free to Contact us via</p>
         <p><strong>Email:</strong> wastecollectionsystem.lk@gmail.com</p>
@@ -94,94 +95,106 @@ const CustomContact = () => {
             <FaTiktok size={30} color="#FFFFFF" />
           </a>
         </div>
-        <p className="business-para">Business Hours: Monday - Friday: 9 AM - 5 PM</p>
+        <p className='business-para'>Business Hours: Monday - Friday: 9 AM - 5 PM</p>
       </div>
+      
       <div className="right-section">
         <h2>Ask Anything</h2>
         <Form onSubmit={handleSubmit} noValidate validated={validated}>
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formFirstName">
-              <Form.Label>First Name *</Form.Label>
-              <Form.Control
-                type="text"
-                name="firstName"
-                placeholder="First Name"
-                value={formData.firstName}
-                onChange={handleChange}
-                isInvalid={!!errors.firstName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.firstName}
-              </Form.Control.Feedback>
+              <div className="form-label-wrapper">
+                <Form.Label>First Name *</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  isInvalid={!!errors.firstName} 
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.firstName}
+                </Form.Control.Feedback>
+              </div>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formLastName">
-              <Form.Label>Last Name *</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                placeholder="Last Name"
-                value={formData.lastName}
-                onChange={handleChange}
-                isInvalid={!!errors.lastName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.lastName}
-              </Form.Control.Feedback>
+              <div className="form-label-wrapper">
+                <Form.Label>Last Name *</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  isInvalid={!!errors.lastName} 
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.lastName}
+                </Form.Control.Feedback>
+              </div>
             </Form.Group>
           </Row>
 
           <Row className="mb-3">
             <Form.Group as={Col} controlId="formEmail">
-              <Form.Label>Email *</Form.Label>
-              <Form.Control
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleChange}
-                isInvalid={!!errors.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
+              <div className="form-label-wrapper">
+                <Form.Label>Email *</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  isInvalid={!!errors.email} 
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </div>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formPhone">
-              <Form.Label>Phone Number *</Form.Label>
-              <Form.Control
-                type="tel"
-                name="phone"
-                placeholder="Phone Number"
-                value={formData.phone}
-                onChange={handleChange}
-                isInvalid={!!errors.phone}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.phone}
-              </Form.Control.Feedback>
+              <div className="form-label-wrapper">
+                <Form.Label>Phone Number *</Form.Label>
+                <Form.Control
+                  type="tel"
+                  name="phone"
+                  placeholder="Phone Number"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  isInvalid={!!errors.phone} 
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.phone}
+                </Form.Control.Feedback>
+              </div>
             </Form.Group>
           </Row>
 
           <Form.Group controlId="formMessage" className="mb-3">
-            <Form.Label>Message</Form.Label>
-            <Form.Control
-              as="textarea"
-              name="message"
-              placeholder="Write your inquiry here..."
-              value={formData.message}
-              onChange={handleChange}
-              isInvalid={!!errors.message}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.message}
-            </Form.Control.Feedback>
+            <div className="form-label-wrapper">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="message"
+                placeholder="Write your inquiry here..."
+                value={formData.message}
+                onChange={handleChange}
+                isInvalid={!!errors.message} 
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.message}
+              </Form.Control.Feedback>
+            </div>
           </Form.Group>
 
           <Button variant="success" type="submit">
             Submit
           </Button>
         </Form>
+
         {successMessage && <p className="success-message">{successMessage}</p>}
         {errorMessage && <p className="error-message">{errorMessage}</p>}
       </div>
